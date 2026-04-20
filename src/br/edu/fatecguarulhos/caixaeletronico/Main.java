133dc06d8dd0ff94ca839cae5cb64940a207d519
luciobrito <luciobp869@gmail.com>
2026-04-20 20:17:11 -0300

    Testes classe Main

--- src/br/edu/fatecguarulhos/caixaeletronico/Main.java
+++ src/br/edu/fatecguarulhos/caixaeletronico/Main.java
@@ -5,8 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		CaixaEletronico cx = new CaixaEletronico();
 		
-		System.out.println(cx.sacar(317));
-
+		System.out.println(cx.sacar(308));
+		System.out.println(cx.sacar(311));
+		System.out.println(cx.sacar(313));
 	}
 
 }
